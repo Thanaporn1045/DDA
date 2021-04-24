f5436cf60f2da0cbeb28327387e1597e96723e3e
Thanaporn1045 <78547225+Thanaporn1045@users.noreply.github.com>
2021-04-24 16:06:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, dx, dy, step;
+            float x1, y1, x2, y2;
+            
+            x1 = float.Parse(Console.ReadLine());
+            y1 = float.Parse(Console.ReadLine());
+            x2 = float.Parse(Console.ReadLine());
+            y2 = float.Parse(Console.ReadLine());
+            dx = x2 - x1;
+            dy = y2 - y1;
+            if(Math.Abs(dx)>=Math.Abs(dy))
+            {
+                step = Math.Abs(dx);
+            }
+            else
+            {
+                step = Math.Abs(dy);
+            }
+            dx /= step;
+            dy /= step;
+            x = x1;
+            y = y1;
+            int i;
+            for(i=1; i<=step; i++)
+            {   Console.Write(x);
+                Console.Write(",\",");
+                Console.Write("\",");
+                Console.Write(y);
+                x += dx;
+                y += dy;
+            }
+        }
+    }
+}
